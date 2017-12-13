06bb0a9618b3c1fbd45b1f05be58398ac49388fb
sergeitarasov <sergxf@yandex.ru>
2017-12-13 11:14:08 -0500

    Q matrix pipeline ready

---
+++ rphenoscate/R/Qmatrix-pipeline.R
@@ -0,0 +1,109 @@
+library("magrittr", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+setwd("~/Documents/Phenoscate/Phenoscate-R/rphenoscate/rphenoscate/R")
+
+
+#' @title Combining two matrices
+#' @description Combining two matrices. The parametric schem of matrice is defined by nattural
+#' numbers; different numbers = different rate parameters
+#' @param M1 matrix; if dependency true thenM1 controls M2
+#' @param M2 matrix; if dependency true then: M2 depends on those states of M1 specified in dependent.state
+#' @param dependent.state state(s) of M1 that switch on matrix M2 
+#' @param name.sep separator for state names
+#' @param diag.as hpopulate main diagonal with
+#' @return Matrix
+#' @examples
+#' M1<-matrix(c(-1,1,  2,-2),2,2,byrow=TRUE)
+#' rownames(M1)<-colnames(M1)<-c("0","1")
+#' M2<-matrix(c(-3,3,  4,-4),2,2,byrow=TRUE)
+#' rownames(M2)<-colnames(M2)<-c("0","1")
+#' comb2matrices(M1, M2, dependent.state=NULL)
+#' comb2matrices(M1, M2, dependent.state=2)
+
+# if dependency true then: M2 depends on M1 states specified in dependent.state
+comb2matrices<-function(M1,M2, dependent.state=NULL, name.sep="", diag.as=""){
+  
+  if (!is.null(dependent.state)){
+    matrix.diag<-rep(0, ncol(M1))
+    matrix.diag[dependent.state]<-1
+    matrix.diag<-diag(matrix.diag)
+  }
+  
+  if (is.null(dependent.state)){
+    matrix.diag<-diag(nrow(M1))
+  }
+  
+  M_kr=(M1%x%diag(nrow(M2)))+ (matrix.diag%x%M2)
+  
+  
+  #getting colnames
+  
+  col=paste(colnames(kronecker(M1, diag(nrow(M2)), make.dimnames = T)),
+            colnames(kronecker(diag(nrow(M1)), M2, make.dimnames = T)), sep="")
+  col=gsub("::", name.sep, col, fixed=T)
+  
+  # merging two names
+  rownames(M_kr)<-colnames(M_kr)<-col
+  if (diag.as!="") diag(M_kr)<-diag.as
+  
+  return(M_kr)
+}
+
+######################################
+
+
+
+
+
+# read dependecy table
+chrs.depen<-read.csv("dependencies.txt", header=T,  stringsAsFactors = F, na.strings = "")
+
+# get a dependent pair of chars
+char.id<-2
+seq.of.chars<-which(chrs.depen[,1]==char.id)
+seq.of.chars<-chrs.depen[seq.of.chars,3]%>% unique()
+# first chrs in the vecto depends on the following chars
+seq.of.chars<-c(char.id, seq.of.chars)
+
+#intialize N matrices where N=number of binary chars involved in a given dependecy scheme,
+# all matrices are populated with different parameters
+# this can be better done using array()
+matrix.list=list()
+#
+# each row is the id of rate parameters which will be plugged in rate matrices
+param.scheme<-matrix(seq(1:(2*length(seq.of.chars))), ncol = 2, byrow=TRUE)
+for (i in seq_along(seq.of.chars)){
+  matrix.list[[i]]<-matrix(c(-param.scheme[i,1],param.scheme[i,1], 
+                             param.scheme[i,2],-param.scheme[i,2]),2,2,byrow=TRUE)
+  rownames(matrix.list[[i]])<-colnames(matrix.list[[i]])<-c("0","1")
+}
+
+# combine matrices together given their dependecies
+# this matrix can be used for inference with hidden models
+combined.matrix=comb2matrices(matrix.list[[2]], matrix.list[[1]], dependent.state=2, diag.as=0)
+
+# check lumpability in respect to given partitioning scheme for states reflecting meaningfull
+# absence, so we can use non-hidden models
+# I will work on this function to more to get it working properly
+#part_scheme=list(c(1, 2), c(3), c(4))
+#is_strg_lumpable(combined.matrix, part_scheme)
+# if matrix is lumpable then we construct the aggregated chain, let's imagine the matrix
+# is lumpable
+is.lumpable=T
+
+if (is.lumpable==T) {
+  #identify which states are not meaningfull given absence
+  # I will work on it to extend for arbitrary number of combined matrices
+  # by far I just create a matrix how it should look like
+  aggregated.matrix<-matrix(c(0,1,2,  3,0,4, 5, 6, 0),3,3,byrow=TRUE)
+  rownames(aggregated.matrix)<-colnames(aggregated.matrix)<-c("0","10", "11")
+
+}
+
+### Outputs
+combined.matrix # combined matrix
+aggregated.matri # aggregated matrix
+seq.of.chars # pair of chars where #1 depends on #2 (swith off dependency), also useful to 
+#know character order in state names
+
+
+
